a4519b0ea96272c0d3b21a72f1dafe6fb5cb09d0
Cmape <carlosmape69@gmail.com>
2022-02-08 19:30:18 -0500

    Se añadio un nuevo archivo

---
+++ index.js
@@ -0,0 +1,10 @@
+let saludo = 1;
+//console.log('El resultado es:');
+//console.log(a);
+
+if (saludo > 1) {
+saludo = 'Es mayor';
+} else {
+    saludo = 'Su numero no es mayor';
+}
+console.log(saludo);
---
+++ index.py
@@ -0,0 +1,3 @@
+a = 1
+print ('El resultado es:')
+print (a)
